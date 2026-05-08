1fbbfe9cec7a74542745f84843226816884e5d2a
mahirdigital5-cmd <mahirdigital5@gmail.com>
2026-05-08 21:26:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -94,19 +94,23 @@ async function startBot() {
       // =========================
 
       if (found.image) {
-        await sock.sendMessage(msg.key.remoteJid, {
-          image: { url: found.image },
-          caption: found.response || "",
-        });
+  const response = await fetch(found.image);
+  const arrayBuffer = await response.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
 
-        console.log("GAMBAR DIKIRIM:", found.image);
-      } else {
-        await sock.sendMessage(msg.key.remoteJid, {
-          text: found.response,
-        });
+  await sock.sendMessage(msg.key.remoteJid, {
+    image: buffer,
+    caption: found.response || "",
+  });
 
-        console.log("BALASAN DIKIRIM:", found.response);
-      }
+  console.log("GAMBAR DIKIRIM:", found.image);
+} else {
+  await sock.sendMessage(msg.key.remoteJid, {
+    text: found.response,
+  });
+
+  console.log("BALASAN DIKIRIM:", found.response);
+}
 
     } catch (err) {
       console.log("ERROR:", err);
